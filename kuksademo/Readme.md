0d69cf7c6cec0c96614267fadd69a5c5b9faa7f0
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2025-10-24 12:25:08 +0200

    Live demo playbook
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

---
+++ kuksademo/Readme.md
@@ -0,0 +1,3 @@
+# KUKSA live demo notes
+
+This is not a full "project" but some notes to replicate the mini demos we sometimes give when introducing KUKSA, such as https://covesa.atlassian.net/wiki/download/attachments/39067416/Day1_1000_Schildt_VSSIntro.pdf?version=2&modificationDate=1747839614062&cacheVersion=1&api=v2
---
+++ kuksademo/data-provider.py
@@ -0,0 +1,59 @@
+# Example provider
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import random
+
+from kuksa_client.grpc import Datapoint
+from kuksa_client.grpc import VSSClient
+
+
+def updateValue(client, path, value):
+    print(f"Updating {path} to: {value}")
+    client.set_current_values(
+        {path: Datapoint(value)})
+
+
+def choosePad():
+    user_input = input("Choose a brake pad (FL, FR, RL, RR): ").upper()
+    if user_input == "FL":
+        return "Vehicle.Chassis.Axle.Row1.Wheel.Left.Brake.PadWear"
+    elif user_input == "FR":
+        return "Vehicle.Chassis.Axle.Row1.Wheel.Right.Brake.PadWear"
+    elif user_input == "RL":
+        return "Vehicle.Chassis.Axle.Row2.Wheel.Left.Brake.PadWear"
+    elif user_input == "RR":
+        return "Vehicle.Chassis.Axle.Row2.Wheel.Right.Brake.PadWear"
+    else:
+        print("Invalid input. Defaulting to FL.")
+        return "Vehicle.Chassis.Axle.Row1.Wheel.Left.Brake.PadWear"
+
+
+def chooseValue():
+    try:
+        user_input = int(input("Choose a value (0-100): "))
+        if 0 <= user_input <= 100:
+            return user_input
+        else:
+            print("Value out of range. Defaulting to 0")
+            return 0
+    except ValueError:
+        print("Invalid input. Using a random value instead.")
+        return random.randint(0, 100)
+
+
+def main():
+    # Create a VSSClient instance, use 55555 when not on MAC
+
+    client = VSSClient('127.0.0.1', 55556)
+    client.connect()
+    while True:
+        # Use user input. In real use cases my process CAN messages or similar
+        path = choosePad()
+        value = chooseValue()
+        updateValue(client, path, value)
+
+
+if __name__ == '__main__':
+    main()
